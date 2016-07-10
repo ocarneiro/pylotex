71bfcd3d2e35a4ce0829355e18a2990aaf77aae1
ocarneiro <otavio@carneiro.blog.br>
2016-07-09 22:03:16 -0300

    Creation

---
+++ pylotex.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+""" pylotex """
+
+# TODO opens TeX file
+
+# TODO creates odt file
+
+# TODO finds \begin{document}
+
+# TODO iterates through TeX file and adds text to odt file
+
+# TODO finds \end{document}
+
+# TODO saves odt file

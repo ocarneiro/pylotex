74a46df1b838d97470223aa8cc4bde85d864d8a7
ocarneiro <otavio@carneiro.blog.br>
2016-07-10 22:02:24 -0300

    First tests using pytest

---
+++ odt.py
@@ -0,0 +1,6 @@
+from odf.opendocument import OpenDocumentText as ODT
+
+class Odt(object):
+
+    def __init__(self):
+        pass
---
+++ test_pylotex.py
@@ -0,0 +1,6 @@
+from odt import Odt
+
+def test_generate_odt():
+    """Tests if an odt file is properly generated"""
+    odt = Odt()
+

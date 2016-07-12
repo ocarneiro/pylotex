945a5f558eaa1f6b7365ac3432211d52b7c851ad
ocarneiro <otavio@carneiro.blog.br>
2016-07-11 21:09:42 -0300

    Initial text addition

--- odt.py
+++ odt.py
@@ -1,6 +1,10 @@
 from odf.opendocument import OpenDocumentText as ODT
 
+
 class Odt(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, generator=None):
+        self.generator = generator or ODT()
+
+    def add_text(self, text):
+        self.generator.text.addElement(text)
--- test_pylotex.py
+++ test_pylotex.py
@@ -1,6 +1,14 @@
 from odt import Odt
+from unittest import mock
+from odf.opendocument import OpenDocumentText as ODT
 
 def test_generate_odt():
     """Tests if an odt file is properly generated"""
     odt = Odt()
 
+def test_add_text_to_odt():
+    """Tests if add method is called on odt"""
+    mock_generator = mock.MagicMock()
+    odt = Odt(generator=mock_generator)
+    odt.add_text("Testing")
+    mock_generator.text.addElement.assert_called_with("Testing")
